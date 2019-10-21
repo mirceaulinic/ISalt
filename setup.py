d48ee2d06347e0c29fd0e3fb53d045104dce56eb
Mircea Ulinic <mulinic@digitalocean.com>
2019-10-21 11:17:58 +0100

    Release ISalt 2019.10.0b1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("requirements.txt", "r") as fs:
 
 setup(
     name='isalt',
-    version='2019.10.0',
+    version='2019.10.0b1',
     namespace_packages=['isalt'],
     packages=find_packages(),
     author='Mircea Ulinic',
